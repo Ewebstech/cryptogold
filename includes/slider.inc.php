32b805fae8e6e9b4c5b42d2a1f31158fb552d4fd
Emmanuel Paul <e.paul@dreammesh.ng>
2018-12-20 18:42:17 +0100

    updates

--- includes/slider.inc.php
+++ includes/slider.inc.php
@@ -1,77 +1,44 @@
-<div id="shopify-section-slider" class="shopify-section index-section col-lg-9 col-xs-12">
-            <div id="wbslider" style="display:none;" data-version="5.4.3.1">
-              <ul>
-
-                <li>
-
-                  <a class="tp-caption rev-link  tp-resizeme" href="#" data-frames='[{"delay":10,"speed":0},{"delay":"wait","speed":10}]'>
-                    <img src="img/Felicia.jpg" alt="" title="" class="img-responsive">
-                  </a>
-
-                </li>
-
-
-                <li>
-
-                  <a class="tp-caption rev-link  tp-resizeme" href="#" data-frames='[{"delay":10,"speed":0},{"delay":"wait","speed":10}]'>
-                    <img src="img/img1.jpg" alt="" title="" class="img-responsive">
-                  </a>
-
-                </li>
-
-              </ul>
-            </div>
-
-
-            <script type="text/javascript">
-              var revapi71,
-                tpj = jQuery;
-
-              tpj(document).ready(function () {
-                if (tpj("#wbslider").revolution == undefined) {
-                  revslider_showDoubleJqueryError("#wbslider");
-                } else {
-                  revapi71 = tpj("#wbslider").show().revolution({
-                    sliderType: "standard",
-                    sliderLayout: "auto",
-                    dottedOverlay: "none",
-                    delay: 8000,
-                    navigation: {
-                      onHoverStop: "off",
-                      bullets: {
-                        enable: true,
-                        hide_onmobile: false,
-                        style: "hermes",
-                        hide_onleave: false,
-                        direction: "horizontal",
-                        h_align: "center",
-                        v_align: "bottom",
-                        h_offset: 0,
-                        v_offset: 20,
-                        space: 5,
-                        tmp: ''
-                      }
-                    },
-                    gridwidth: 813,
-                    gridheight: 390,
-                    lazyType: "none",
-                    shadow: 0,
-                    spinner: "spinner0",
-                    stopLoop: "off",
-                    stopAfterLoops: -1,
-                    stopAtSlide: -1,
-                    shuffle: "off",
-                    autoHeight: "off",
-                    disableProgressBar: "off",
-                    debugMode: false,
-                    fallbacks: {
-                      simplifyAll: "off",
-                      nextSlideOnWindowFocus: "off",
-                      disableFocusListener: false,
-                    }
-                  });
-                }
-
-              }); /*ready*/
-            </script>
-          </div>
+<section id="home" class="slider-section">
+		<div class="home-slides-2 owl-carousel owl-theme ">
+			<div class="home-single-slide" data-background="assets/img/bg/bg3.jpg">
+				<div class="home-slide-overlay"></div>
+				<div class="home-single-slide-inner">
+					<div class="auto-container">
+						<div class="row">
+							<div class="col-lg-6 col-md-8 col-sm-8 col-12 mx-auto text-center">
+								<div class="home-single-slide-dec">
+									<h2>CryptoWealth</h2>
+									<p>Secure Cryptocurrency Investments &amp; Trading Platform </p>
+									<div class="home-single-slide-button">
+										<a href="#" class="btn-style btn-filled">Contact Us</a>
+										<a href="#" class="btn-style btn-border">Start Now</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- end single slider -->
+			<div class="home-single-slide" data-background="assets/img/bg/slide2.jpg">
+				<div class="home-slide-overlay"></div>
+				<div class="home-single-slide-inner">
+					<div class="auto-container">
+						<div class="row">
+							<div class="col-lg-6 col-md-8 col-sm-8 col-12 mx-auto text-center">
+								<div class="home-single-slide-dec">
+									<h2>Invest with digital currencies</h2>
+									<p>Invest Now. Harvest Profits</p>
+									<div class="home-single-slide-button">
+										<a href="#" class="btn-style btn-filled">Invest Now</a>
+										<a href="#" class="btn-style btn-border">Ask Questions</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- end single slider -->
+		</div>
+	</section>
