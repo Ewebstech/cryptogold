3fcfab1073eab247543f84731931e44cb6b8a9f4
Emmanuel Paul <e.paul@dreammesh.ng>
2018-12-20 18:46:46 +0100

    updates

---
+++ index.php
@@ -0,0 +1,447 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <title>CryptoWealth | Home</title>
+    <!-- Bootstrap core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Font  -->
+    <link href="http://fonts.googleapis.com/css69f2.css?family=Montserrat:400,400i,500,500i,600,600i,700,700i|Roboto:400,400i,500,500i,700,700i" rel="stylesheet"> 
+    <!-- icofont icon -->
+    <link rel="stylesheet" href="assets/css/icofont.css">	
+    <!-- font awesome icon -->
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">	
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+	<!--- meanmenu Css-->
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css" media="all" />	
+	<?php include("includes/header.inc.php"); ?>
+	
+	<!-- START SLIDER SECTION -->
+	<?php include("includes/slider.inc.php"); ?>
+	<!-- END SLIDER SECTION  -->
+	
+	<!-- START ABOUT SECTION -->
+    <section id="about2" class="section-padding pt-lg-0 pt-md-0">
+        <div class="container">
+			<div class="row text-center">
+				<div class="single-about-item col-lg-3 col-md-3 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-about">
+						<div class="single-about-img">
+							<div class="single-about-img-inner">
+								<a href="#"><img src="assets/img/bg/bg1.jpg" class="img-fluid" alt=""></a>
+							</div>
+						</div>
+						<div class="single-about-caption">
+							<div class="single-about-icon">
+								<i class="fa fa-user-plus"></i>
+							</div>
+							<div class="single-about-title">
+								<h5>Get An Account</h5>
+							</div>
+							<div class="single-about-dec">
+								<p>Setup a FREE account in minutes & get started earning your passive Bitcoin Income.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end single about -->
+				<div class="single-about-item col-lg-3 col-md-3 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-about">
+						<div class="single-about-img">
+							<div class="single-about-img-inner">
+								<a href="#"><img src="assets/img/bg/bg4.jpg" class="img-fluid" alt=""></a>
+							</div>
+						</div>
+						<div class="single-about-caption">
+							<div class="single-about-icon">
+								<i class="fab fa-bitcoin"></i>
+							</div>
+							<div class="single-about-title">
+								<h5>Deposit</h5>
+							</div>
+							<div class="single-about-dec">
+								<p>Take a look at the plans and decide how much you want to invest then make your deposit.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end single about -->
+				<div class="single-about-item col-lg-3 col-md-3 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-about">
+						<div class="single-about-img">
+							<div class="single-about-img-inner">
+								<a href="#"><img src="assets/img/bg/bg5.jpg" class="img-fluid" alt=""></a>
+							</div>
+						</div>
+						<div class="single-about-caption">
+							<div class="single-about-icon">
+								<i class="fa fa-chart-bar"></i>
+							</div>
+							<div class="single-about-title">
+								<h5>Earn</h5>
+							</div>
+							<div class="single-about-dec">
+								<p>Once your deposit is active, our traders and automated system will go to work for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- end single about -->
+				<div class="single-about-item col-lg-3 col-md-3 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-about">
+						<div class="single-about-img">
+							<div class="single-about-img-inner">
+								<a href="#"><img src="assets/img/bg/bg6.jpg" class="img-fluid" alt=""></a>
+							</div>
+						</div>
+						<div class="single-about-caption">
+							<div class="single-about-icon">
+								<i class="fab fa-gg"></i>
+							</div>
+							<div class="single-about-title">
+								<h5>Withdraw</h5>
+							</div>
+							<div class="single-about-dec">
+								<p>All payout requests are handled instantly and automatically by our system.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			</div>
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END ABOUT SECTION -->
+	
+	<!-- START PROMO SECTION -->
+    <section id="promo" class="section-padding bg-gray">
+        <div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="row">
+						<div class="col-12 text-left">
+							<div class="section-title-2">
+								<h3>Frequently <span>questions</span></h3>
+							</div>
+						</div>
+					</div>
+					<!-- end section title -->
+					
+                    <!--Accordion Box-->
+                    <ul class="accordion-box">
+                        <li class="accordion block">
+                            <div class="acc-btn">
+                                <div class="icon-outer"><span class="icon fa fa-user-plus"></span></div>How can I Register?</div>
+                            <div class="acc-content">
+                                <div class="content">
+                                    <p>You can register by clicking the 'Get Started' button anywhere on the website.</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="accordion block">
+                            <div class="acc-btn active">
+                                <div class="icon-outer"><span class="icon icofont icofont-ui-press"></span></div>How secure is my investment?</div>
+                            <div class="acc-content current">
+                                <div class="content">
+                                    <p>Our system is transparent, solid and reliable. We have traded and earned investors profit over the years and have hence gained credibility.</p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!--Block-->
+                        <li class="accordion block">
+                            <div class="acc-btn">
+                                <div class="icon-outer"><span class="icon icofont icofont-ui-press"></span></div>When do we close?</div>
+                            <div class="acc-content">
+                                <div class="content">
+                                    <p>You can Invest at anytime of the day, month or year. We are also always available to answer your questions as neccessary.</p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!--Block-->
+                        <li class="accordion block">
+                            <div class="acc-btn">
+                                <div class="icon-outer"><span class="icon icofont icofont-ui-press"></span></div>What if I decide to stop investing with you</div>
+                            <div class="acc-content">
+                                <div class="content">
+                                    <p>No problems. We would ask a few questions, provide you your full entitlements and let you be on your way.</p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!--Block-->
+                        <li class="accordion block">
+                            <div class="acc-btn">
+                                <div class="icon-outer"><span class="icon icofont icofont-ui-press"></span></div>How can i get the price idea?</div>
+                            <div class="acc-content">
+                                <div class="content">
+                                    <p>Our pricing is stated clearly on the website. Surf through to get appropriate information.</p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!--Block-->
+                        <li class="accordion block">
+                            <div class="acc-btn">
+                                <div class="icon-outer"><span class="icon icofont icofont-ui-press"></span></div>How can I contact you?</div>
+                            <div class="acc-content">
+                                <div class="content">
+                                    <p>You can contact us by clicking the 'contact' button anywhere on the website.</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <!--End Accordion Box-->	
+				</div>
+				<!-- end col -->
+				<div class="col-lg-6 col-md-6 col-12">
+					<div class="row">
+						<div class="col-12 text-left">
+							<div class="section-title-2">
+								<h3>About <span>Us</span></h3>
+							</div>
+						</div>
+					</div>
+					<!-- end section title -->
+					<div class="image-style-1 mb-4">
+						<div class="image-style-wrapper">
+							<div class="image-one"></div>
+							<div class="image-two">
+								<img src="assets/img/bitcoin-banner.jpg" class="img-fluid" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="promo-content">
+						<h4>Who we are?</h4>
+						<p>Founded in June of 2016, <b>CryptoWealth's</b> longevity and success is due to a relentless pursuit of accuracy, depth and innovation, enhanced by the wisdom of past experience and a solid grasp on the future ahead. More than its individual brands, <b>CryptoWeatlh</b> is a digital currency wallet and platform where investors can transact with new digital currency e.g Bitcoin. We work with large brokerage houses based in New-York, Chicago and London, Our professional and experienced traders get extremely good and stable results in money management activities. We are a modern gateway to intelligence, with innovative technology, advanced data feeds, integrated solutions and expert research. </p>
+						
+					</div>	
+				</div>
+				<!-- end col -->				
+			</div>
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END PROMO SECTION -->
+	
+	<!-- START CHOOSE US SECTION -->
+    <section id="chhoseus" class="section-padding">
+        <div class="container">
+			<div class="row">
+				<div class="col-12 text-center">
+					<div class="section-title">
+						<h5>CryptoWealth</h5>
+						<h3>Why Choose <span>CryptoWealth</span></h3>
+					</div>
+				</div>
+			</div>
+			<!-- end section title -->
+			<div class="row">
+				<div class="col-lg-8 col-md-8 col-12 mb-lg-0 mb-md-0 mb-5">
+					<div class="row">	 
+						<div class="col-lg-6 col-md-6 col-12 mb-5">
+							<div class="single-wcus">
+								<div class="single-wcus-icon">
+									<i class="fa fa-phone" style='font-size: 30px; margin-bottom: 16px; margin-left: 6px;'></i>
+								</div>
+								<div class="single-wcus-text">
+									<h5>24/7 Support</h5>
+									<p>CryptoWealth call center staff and customer service managers are ready to answer any questions from investors around the clock – after all, we work with investors from all over the globe.</p>
+								</div>
+							</div>
+						</div>
+						<!-- end single chooseus -->	 
+						<div class="col-lg-6 col-md-6 col-12 mb-5">
+							<div class="single-wcus">
+								<div class="single-wcus-icon">
+									<i class="icofont icofont-coins"></i>
+								</div>
+								<div class="single-wcus-text">
+									<h5>Better Profits</h5>
+									<p>CryptoWealth offers the most favorable investment terms with regard to the income level and risk minimization, guaranteeing really significant profits.</p>
+								</div>
+							</div>
+						</div>
+						<!-- end single chooseus -->	 
+						<div class="col-lg-6 col-md-6 col-12 mb-lg-0 mb-md-0 mb-5">
+							<div class="single-wcus">
+								<div class="single-wcus-icon">
+									<i class="icofont icofont-ui-travel"></i>
+								</div>
+								<div class="single-wcus-text">
+									<h5>Security</h5>
+									<p>CryptoWealth goal is to secure the investors’ funds at the highest level, it is the same as keeping your funds in a bank. The system will allow a user to operate with the account only after authorization.</p>
+								</div>
+							</div>
+						</div>
+						<!-- end single chooseus -->	 
+						<div class="col-lg-6 col-md-6 col-12">
+							<div class="single-wcus">
+								<div class="single-wcus-icon">
+									<i class="icofont icofont-money"></i>
+								</div>
+								<div class="single-wcus-text">
+									<h5>Speed</h5>
+									<p>The system enables quick actions. When you ask for withdrawal of funds, it is performed instantly & does not involve any human interference.</p>
+								</div>
+							</div>
+						</div>
+						<!-- end single chooseus -->
+					</div>
+				</div>
+				<!-- end col -->	 
+				<div class="col-lg-4 col-md-4 col-12">
+					<div class="single-wcus-promo">
+						<div class="single-wcus-promo-inner">
+							<h3 style="text-align: center">We are ready to fly with you.</h3>
+							<p>Our Mission is to service our customers by creating and delivering quality service, which is unique, valuable and profitable to help our clients attain financial success by providing real investment opportunities and support. </p>
+							<center><a href="#" class="btn-style btn-border btn-border-2 text-center"><i class="fa fa-check-circle"></i> Start Now</a></center>
+						</div>
+					</div>
+				</div>
+				<!-- end col -->	
+			</div>		
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END CHOOSE US SECTION -->
+	
+	<!-- START PRICE SECTION -->
+    <section id="price" class="section-padding bg-gray">
+        <div class="container">
+			<div class="row">
+				<div class="col-12 text-center">
+					<div class="section-title">
+						<h5>Bitfonix packages</h5>
+						<h3>Our <span>Pricing</span></h3>
+					</div>
+				</div>
+			</div>
+			<!-- end section title -->
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-12 mb-lg-0 mb-md-0 mb-5">
+					<div class="single-pricing">
+						<div class="single-pricing-head text-center">
+							<span class="price"><sup>$</sup><span class="price-digit">200</span>/30 days</span>
+							<h5>Starter</h5>
+						</div>
+						<div class="single-pricing-body">
+							<div class="single-pricing-list">
+								<ul>
+									<li><i class="icofont icofont-check-alt"></i> 35% Interest Rate</li>
+									<li><i class="icofont icofont-check-alt"></i> 99.9% Uptime</li>
+									<li><i class="icofont icofont-check-alt"></i> Free Support</li>
+								</ul>
+							</div>
+						</div>
+						<div class="single-pricing-button">
+							<a href="#" class="mt-3 d-block text-center rounded-0">Invest Now</a>
+						</div>	
+					</div>
+				</div>
+				<!-- end col -->
+				<div class="col-lg-4 col-md-4 col-12 mb-lg-0 mb-md-0 mb-5">
+					<div class="single-pricing">
+						<div class="single-pricing-head text-center">
+							<span class="price"><sup>$</sup><span class="price-digit">500</span>/30 days</span>
+							<h5>Standard</h5>
+						</div>
+						<div class="single-pricing-body">
+							<div class="single-pricing-list">
+								<ul>
+									<li><i class="icofont icofont-check-alt"></i> 40% Interest</li>
+									<li><i class="icofont icofont-check-alt"></i> 99.9% Uptime</li>
+									<li><i class="icofont icofont-check-alt"></i> Free Support</li>
+								</ul>
+							</div>
+						</div>
+						<div class="single-pricing-button">
+							<a href="#" class="mt-3 d-block text-center rounded-0">Invest Now</a>
+						</div>	
+					</div>
+				</div>
+				<!-- end col -->
+				<div class="col-lg-4 col-md-4 col-12">
+					<div class="single-pricing">
+						<div class="single-pricing-head text-center">
+							<span class="price"><sup>$</sup><span class="price-digit">1000</span>/30 days</span>
+							<h5>Professional</h5>
+						</div>
+						<div class="single-pricing-body">
+							<div class="single-pricing-list">
+								<ul>
+									<li><i class="icofont icofont-check-alt"></i> 50% Interest</li>
+									<li><i class="icofont icofont-check-alt"></i> 99.9% Uptime</li>
+									<li><i class="icofont icofont-check-alt"></i> Extra Support</li>
+								</ul>
+							</div>
+						</div>
+						<div class="single-pricing-button">
+							<a href="#" class="mt-3 d-block text-center rounded-0">Invest Now</a>
+						</div>	
+					</div>
+				</div>
+				<!-- end col -->
+			</div>
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END PRICE SECTION -->
+	
+	<!-- START CALLTOACTION SECTION -->
+	<section id="calltoaction" class="calltoaction-padding section-padding bg-theme">
+		<div class="auto-container">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="calltoaction-two-wrap">
+						<h4>Still have questions?</h4>
+						<p>It's okay, simply go to our contact page and feel free to ask any questions there.</p>
+					</div>
+				</div>
+				<!-- end col -->
+				<div class="col-lg-4 col-md-4 col-sm-12 col-12 text-lg-right text-md-right text-sm-left text-left">
+					<a href="/contact" class="btn-style btn-border btn-border-3">Contact Us</a>
+				</div>
+				<!-- end col -->
+			</div>
+			<!-- end row-->
+		</div>
+	</section>
+    <!-- END CALLTOACTION SECTION -->
+
+
+
+	<!-- START CLIENT SECTION -->
+    <section id="client" class="client-section-padding bg-gray">
+        <div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<div class="client-slider owl-carousel owl-theme">
+						<div class="single-client">
+                            <a href="#"><img class="img-fluid" src="assets/img/secured.PNG" alt=""></a>
+                        </div>
+						<div class="single-client">
+                            <a href="#"><img class="img-fluid" src="assets/img/secured (1).PNG" alt=""></a>
+                        </div>
+						<div class="single-client">
+                            <a href="#"><img class="img-fluid" src="assets/img/secured (2).PNG" alt=""></a>
+                        </div>
+						
+					</div>
+				</div>
+			</div>
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END CLIENT SECTION -->
+	
+	<?php include("includes/footer.inc.php"); ?>
+</body>
+
+</html>
